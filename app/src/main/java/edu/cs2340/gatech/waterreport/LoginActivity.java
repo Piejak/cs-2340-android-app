577e556de913f8dfc4cf2c50fa515e124d3085b0
Brian Piejak <Brian.Piejak@gmail.com>
2017-02-10 15:59:52 -0500

    Add landing page and set intent to open when the login is successful

--- app/src/main/java/edu/cs2340/gatech/waterreport/LoginActivity.java
+++ app/src/main/java/edu/cs2340/gatech/waterreport/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.cs2340.gatech.waterreport;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -185,7 +186,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(email, password, this);
             mAuthTask.execute((Void) null);
         }
     }
@@ -298,10 +299,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        // need to give the inner class the parent activity to call an intent from inside this inner
+        // class
+        private final LoginActivity mActivity;
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(String email, String password, LoginActivity activity) {
             mEmail = email;
             mPassword = password;
+            mActivity = activity;
         }
 
         @Override
@@ -333,6 +338,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent(mActivity, LandingActivity.class);
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
