8cf25361d2579a434f2805434b36b81d0b30b131
Brian Piejak <Brian.Piejak@gmail.com>
2017-02-10 21:32:12 -0500

    Set up external registration and login with Firebase

--- app/src/main/java/edu/cs2340/gatech/waterreport/LandingActivity.java
+++ app/src/main/java/edu/cs2340/gatech/waterreport/LandingActivity.java
@@ -1,13 +1,26 @@
 package edu.cs2340.gatech.waterreport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private FirebaseUser mUser;
+    private TextView mLandingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        mLandingText = (TextView) findViewById(R.id.landing_text);
+        Intent intent = getIntent();
+        String uid = intent.getStringExtra("FIREBASE_UID");
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mLandingText.setText(mUser.getEmail() + " was successfully logged in.");
     }
 }
