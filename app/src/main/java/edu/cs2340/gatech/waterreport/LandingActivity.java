2c651395c7e720ba470a8ae1125063e3ec6405ae
Brian Piejak <Brian.Piejak@gmail.com>
2017-02-12 21:09:17 -0500

    Add logout button

--- app/src/main/java/edu/cs2340/gatech/waterreport/LandingActivity.java
+++ app/src/main/java/edu/cs2340/gatech/waterreport/LandingActivity.java
@@ -3,6 +3,8 @@ package edu.cs2340.gatech.waterreport;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,6 +14,7 @@ public class LandingActivity extends AppCompatActivity {
 
     private FirebaseUser mUser;
     private TextView mLandingText;
+    private Button mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,12 @@ public class LandingActivity extends AppCompatActivity {
         String uid = intent.getStringExtra("FIREBASE_UID");
         mUser = FirebaseAuth.getInstance().getCurrentUser();
         mLandingText.setText(mUser.getEmail() + " was successfully logged in.");
+        mLogout = (Button) findViewById(R.id.logout_button);
+    }
+
+    public void logoutButtonPressed(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        FirebaseAuth.getInstance().signOut();
+        startActivity(intent);
     }
 }
